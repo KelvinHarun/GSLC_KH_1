55113249ad59b2630a19bc0ad3ce3289d4f0f5f3
KelvinHarun <kelvinharun54@gmail.com>
2025-09-01 05:07:29 +0700

    Update Kelvin-satu.c

--- Kelvin-satu.c
+++ Kelvin-satu.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("Halo sir , my name is Kelvin Harun \n");
+    printf("Hello sir , my name is Kelvin Harun \n");
     printf("And my favorite language is C ");
 
     return 0;
