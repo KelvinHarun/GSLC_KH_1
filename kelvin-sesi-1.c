d03474424129bf69db4c05762e513d39da7da093
Kelvin Harun <Kelvinharun54@gmail.com>
2025-09-06 09:49:23 +0700

    cari area tanpa input

---
+++ kelvin-sesi-1.c
@@ -0,0 +1,19 @@
+//Tugas Area of Rectangle
+
+//Create an algorithm pseudo-code to calculate area of rectangle with following formula
+#include<stdio.h>
+int main() 
+{
+    int length;
+    int width;
+    int area;
+
+    length = 5 ;
+    width  = 2 ;
+
+    area = length * width ;
+    printf("the area is = %d \n\n",area);
+    
+    return 0 ;
+}
+
--- kelvin-sesi-2.c
+++ kelvin-sesi-2.c
@@ -1,22 +1,5 @@
 //Tugas Area of Rectangle
 
-//Create an algorithm pseudo-code to calculate area of rectangle with following formula
-#include<stdio.h>
-int main() 
-{
-    int length;
-    int width;
-    int area;
-
-    length = 5 ;
-    width  = 2 ;
-
-    area = length * width ;
-    printf("the area is = %d \n\n",area);
-    
-    return 0 ;
-}
-
 //Create an algorithm pseudo-code to calculate an area of rectangle with the same formula of previous example, with new condition where the length and width is given by user (input).
    #include<stdio.h>
     int main() 
